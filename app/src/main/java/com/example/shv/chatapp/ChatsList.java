2e24b0d7db82ada23bcb2a7e9c5a221ba23352b9
Luffy <shvmain69@gmail.com>
2019-06-20 22:06:28 +0530

    App Bar and few rearrangements

--- app/src/main/java/com/example/shv/chatapp/ChatsList.java
+++ app/src/main/java/com/example/shv/chatapp/ChatsList.java
@@ -1,10 +1,14 @@
 package com.example.shv.chatapp;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,20 +18,26 @@ import com.example.shv.chatapp.ChatList.ChatListItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ChatsList extends Activity {
+
+public class ChatsList extends AppCompatActivity {
     public List<ChatListItem> chats;
     private RecyclerView chatList;
     private ChatListAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chats_list);
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        setResult(R.integer.notLogOut, intent);
+        finish();
+    }
 
+    private void initChatList() {
         chats = new ArrayList<>();
         chats.add(new ChatListItem("Srikanth", "Bye"));
         chats.add(new ChatListItem("Srihari", "Hi"));
-        chatList = findViewById(R.id.chatListView);
+    }
+
+    private void addAdapter() {
         chatList.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         adapter = new ChatListAdapter(getApplication(), chats);
         chatList.setAdapter(adapter);
@@ -43,5 +53,39 @@ public class ChatsList extends Activity {
 
             }
         }));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats_list);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        chatList = findViewById(R.id.chatListView);
+
+        initChatList();
+        addAdapter();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.chatlistmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                Intent intent = new Intent();
+                setResult(R.integer.LogOut, intent);
+                finish();
+                return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/shv/chatapp/LoginActivity.java
+++ app/src/main/java/com/example/shv/chatapp/LoginActivity.java
@@ -2,11 +2,13 @@ package com.example.shv.chatapp;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.support.v7.widget.Toolbar;
 
 import com.example.shv.chatapp.ChatList.ChatListItem;
 
@@ -18,6 +20,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Login");
+
         ChatListItem.defaultImage = BitmapFactory.decodeResource(getResources(), R.drawable.user);
         username = findViewById(R.id.logInUserName);
         password = findViewById(R.id.logInPassword);
@@ -26,6 +33,13 @@ public class LoginActivity extends AppCompatActivity {
 
     public void logInClick(View v) {
         Intent i = new Intent(getApplicationContext(), ChatsList.class);
-        startActivity(i);
+        startActivityForResult(i, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == 0) && resultCode == R.integer.notLogOut)
+            finish();
     }
 }
